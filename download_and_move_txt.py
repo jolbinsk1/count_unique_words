44e4d257c910dcb86e7e527b32a5ad3fbaa387bf
Julian Olbinski <163324229+jolbinsk1@users.noreply.github.com>
2024-07-18 08:08:13 +0200

    Create download_and_move_txt.py

---
+++ download_and_move_txt.py
@@ -0,0 +1,11 @@
+# First navigate to this repository and download the text file below which contains a comprehensive list of english words:
+  # https://github.com/dwyl/english-words/blob/master/words.txt
+
+# then move to your current working directory
+
+import shutil 
+import os
+
+# shutil.move('the path to the words.txt download', os.getcwd()+'words.txt')
+
+
